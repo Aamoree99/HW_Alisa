8de25efc4337a530b217c09d0fe94236c9f0d96a
Aamoree99 <126795609+Aamoree99@users.noreply.github.com>
2025-02-17 22:44:30 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.menu a').forEach(link => {
+  link.addEventListener('mouseover', () => {
+    link.style.fontWeight = 'bold';
+  });
+  link.addEventListener('mouseout', () => {
+    link.style.fontWeight = 'normal';
+  });
+});
+
+// Делаем плавное появление карточек при загрузке
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".card").forEach((card, index) => {
+    setTimeout(() => {
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }, index * 200);
+  });
+});
